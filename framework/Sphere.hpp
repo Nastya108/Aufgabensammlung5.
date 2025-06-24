ce93378b463239f966bbfa0d259cbb99c3998b52
Nastya108 <nastyatulasi@gmail.com>
2025-06-24 15:01:52 +0200

    implement Sphere.hpp constructor with name_ and color_

--- framework/Sphere.hpp
+++ framework/Sphere.hpp
@@ -3,13 +3,15 @@
 
 // Sphere class inherits from Shape
 class Sphere : public Shape {
+
 private:
-    glm::vec3 center; // Center of the sphere
-    double radius;    // Radius of the sphere
+    glm::vec3 center_;
+    double radius_;
 
 public:
     // Constructor to initialize center and radius
-    Sphere(const glm::vec3& center, double radius);
+    Sphere(const std::string& name, const glm::vec3& color,
+        const glm::vec3& center, double radius);
 
     // Override area and volume methods
     double area() const override;

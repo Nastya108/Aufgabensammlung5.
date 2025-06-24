8c6f280df72f0ce445ff5002b855c023dfc0dd97
Nastya108 <nastyatulasi@gmail.com>
2025-06-24 15:01:24 +0200

    implement Shape.hpp constructor with name_ and color_

--- framework/Shape.hpp
+++ framework/Shape.hpp
@@ -2,10 +2,20 @@
 #define SHAPE_HPP
 
 #include <string>
+#include <glm/vec3.hpp>
 
 // Abstract base class for all 3D shapes
 class Shape {
+
+protected:
+    std::string name_;
+    glm::vec3 color_;
+
 public:
+
+    Shape(const std::string& name, const glm::vec3& color)
+        : name_(name), color_(color) {}
+
     virtual ~Shape() {} // Virtual destructor 
 
     // Pure virtual functions

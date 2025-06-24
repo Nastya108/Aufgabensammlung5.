e77cdd7c938acfafb044bfe7ba8b094d9bbb05d5
Nastya108 <nastyatulasi@gmail.com>
2025-06-24 15:00:54 +0200

    implement Sphere.cpp constructor with name_ and color_

--- framework/Sphere.cpp
+++ framework/Sphere.cpp
@@ -5,16 +5,18 @@
 const double PI = 3.14159265358979323846;
 
 // Constructor: initialize sphere with center and radius
-Sphere::Sphere(const glm::vec3& center, double radius)
-    : center(center), radius(radius) {
+Sphere::Sphere (const std::string& name, const glm::vec3& color,
+    const glm::vec3& center, double radius)
+    : Shape(name, color), center_(center), radius_(radius) {
 }
+
 
 // Surface area of the sphere
 double Sphere::area() const {
-    return 4.0 * PI * radius * radius;
+    return 4.0 * PI * radius_ * radius_;
 }
 
 // Volume of the sphere
 double Sphere::volume() const {
-    return (4.0 / 3.0) * PI * radius * radius * radius;
+    return (4.0 / 3.0) * PI * radius_ * radius_ * radius_;
 }

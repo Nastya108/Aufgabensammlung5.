d08784956807cac9d822f78c3515fc014695e8b6
Nastya108 <nastyatulasi@gmail.com>
2025-06-30 11:12:49 +0200

    Add print method override to Box and fix inheritance from Shape

--- framework/Box.hpp
+++ framework/Box.hpp
@@ -4,13 +4,15 @@
 // Box class inherits from Shape
 class Box : public Shape {
 private:
-    glm::vec3 min; // Minimum corner
-    glm::vec3 max; // Maximum corner
+    glm::vec3 min_; // Minimum corner
+    glm::vec3 max_; // Maximum corner
 
 public:
-    // Constructor: two corners of the box
+    // Constructor: two corners of the box, name and color
     Box(const std::string& name, const glm::vec3& color,
-        const glm::vec3& min_, const glm::vec3& max_);
+        const glm::vec3& min, const glm::vec3& max);
+
+    std::ostream& print(std::ostream& os) const override;
 
     // Override area and volume methods
     double area() const override;

2dc2ab41c0bd85d94a07f39f712fba9875a93240
Nastya108 <nastyatulasi@gmail.com>
2025-06-30 11:04:34 +0200

    Add print method override to Box and fix inheritance from Shape

--- framework/Box.cpp
+++ framework/Box.cpp
@@ -1,26 +1,34 @@
 #include "Box.hpp"
 #include <cmath>
 
-// Constructor: initialize min and max points
+// Constructor
 Box::Box(const std::string& name, const glm::vec3& color,
-    const glm::vec3& min_, const glm::vec3& max_)
-    : Shape(name, color), min(min_), max(max_) {
+    const glm::vec3& min, const glm::vec3& max)
+    : Shape(name, color), min_(min), max_(max) {
 }
 
 // Compute surface area of the box
 double Box::area() const {
-    double dx = max.x - min.x;
-    double dy = max.y - min.y;
-    double dz = max.z - min.z;
+    double dx = max_.x - min_.x;
+    double dy = max_.y - min_.y;
+    double dz = max_.z - min_.z;
 
     return 2.0 * (dx * dy + dy * dz + dz * dx);
 }
 
 // Compute volume of the box
 double Box::volume() const {
-    double dx = max.x - min.x;
-    double dy = max.y - min.y;
-    double dz = max.z - min.z;
+    double dx = max_.x - min_.x;
+    double dy = max_.y - min_.y;
+    double dz = max_.z - min_.z;
 
     return dx * dy * dz;
+}
+
+// Print info about the box
+std::ostream& Box::print(std::ostream& os) const {
+    Shape::print(os);
+    os << ", Min: (" << min_.x << ", " << min_.y << ", " << min_.z << ")"
+        << ", Max: (" << max_.x << ", " << max_.y << ", " << max_.z << ")";
+    return os;
 }

622d3a552179a707dfe4e92c797537716ccc17e0
Nastya108 <nastyatulasi@gmail.com>
2025-06-30 11:06:21 +0200

    Add name_ and color_ to Shape, make area/volume pure virtual, add print

--- framework/Shape.hpp
+++ framework/Shape.hpp
@@ -1,6 +1,7 @@
 #ifndef SHAPE_HPP
 #define SHAPE_HPP
 
+#include <ostream>
 #include <string>
 #include <glm/vec3.hpp>
 
@@ -8,20 +9,28 @@
 class Shape {
 
 protected:
-    std::string name_;
-    glm::vec3 color_;
+    std::string name_;  // Name of the shape
+    glm::vec3 color_;   // Color represented as a 3D vector
 
 public:
-
+    // Constructor to initialize name and color
     Shape(const std::string& name, const glm::vec3& color)
-        : name_(name), color_(color) {}
+        : name_(name), color_(color) {
+    }
 
-    virtual ~Shape() {} // Virtual destructor 
+    // Prints shape info to the given output stream
+    virtual std::ostream& print(std::ostream& os) const;
 
-    // Pure virtual functions
+    // Virtual destructor 
+    virtual ~Shape() {} 
+
+    // // Pure virtual methods to compute area and volume
     virtual double area() const = 0;
     virtual double volume() const = 0;
 
 };
 
-#endif 
+// Overload operator<< to print shape using print() method
+std::ostream& operator<<(std::ostream& os, Shape const& s);
+
+#endif

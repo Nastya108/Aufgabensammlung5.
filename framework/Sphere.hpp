843a42cf05683207265cdb34bd7cbd2011c536e5
Nastya108 <nastyatulasi@gmail.com>
2025-06-30 11:05:47 +0200

    Add intersect method and override print in Sphere

--- framework/Sphere.hpp
+++ framework/Sphere.hpp
@@ -1,19 +1,28 @@
 #include "Shape.hpp"
 #include <glm/vec3.hpp>
+#include "Ray.hpp"       
+#include "HitPoint.hpp"
 
-// Sphere class inherits from Shape
 class Sphere : public Shape {
-
 private:
-    glm::vec3 center_;
-    double radius_;
+    glm::vec3 center_; // Center position of the sphere
+    double radius_;   // Radius of the sphere
+
 
 public:
-    // Constructor to initialize center and radius
+    // Constructor to initialize center, radius, name and color
     Sphere(const std::string& name, const glm::vec3& color,
         const glm::vec3& center, double radius);
+
+    //Print method to display sphere details
+    std::ostream& print(std::ostream& os) const override;
 
     // Override area and volume methods
     double area() const override;
     double volume() const override;
+
+
+    // Checks if the given ray intersects with the sphere and returns hit information
+    HitPoint intersect(Ray const& ray) const;
+
 };
